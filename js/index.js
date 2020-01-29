e8a19eafd2e149612a36d7551358b02531c185e9
Mary <marrow@ukr.net>
2020-01-29 20:22:10 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-jQuery.extend(jQuery.easing, {
-	easeInOutCubic: function (x, t, b, c, d) {
-		if ((t/=d/2) < 1) return c/2*t*t*t + b;
-		return c/2*((t-=2)*t*t + 2) + b;
-	}
+$(document).ready(function () {
+	jQuery.extend(jQuery.easing, {
+		easeInOutCubic: function (x, t, b, c, d) {
+			if ((t/=d/2) < 1) return c/2*t*t*t + b;
+			return c/2*((t-=2)*t*t + 2) + b;
+		}
+	});
 });
 
 $(document).ready(function(){
